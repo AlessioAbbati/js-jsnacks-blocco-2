d85d6e983628b7124565860044d416220644a675
AlessioAbbati <alex_117@live.it>
2023-04-18 16:58:29 +0200

    add array with name and surname

---
+++ jsnack2/myscript.js
@@ -0,0 +1,21 @@
+// creo due array, uno per i nomi e uno per i cognomi
+
+let name = [
+    "Jay",
+    "Daisy",
+    "Jordan",
+    "Nick",
+    "Myrtle",
+    "Meyer",
+    "Dan",
+]
+
+let surname = [
+    "Gatsby",
+    "Buchanan",
+    "Baker",
+    "Carraway",
+    "Wilson",
+    "Wolfsheim",
+    "Cody",
+]
