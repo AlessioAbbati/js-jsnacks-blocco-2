1dbe2495e59f087f10975e6b00944f6b4bccddb0
AlessioAbbati <alex_117@live.it>
2023-04-18 16:43:32 +0200

    compare odd number

--- jsnack1/myscript.js
+++ jsnack1/myscript.js
@@ -1,5 +1,10 @@
 let numeriDispari = []; // Creiamo un array vuoto
 
 for (let i = 0; i < 6; i++) {
-    let numeroutente = parseInt(prompt("Inserisci un numero:"));
+    let numeroUtente = parseInt(prompt("Inserisci un numero:"));
+
+    if (numeroUtente % 2 !== 0) {
+        // Verifichiamo se il numero inserito è dispari
+        numeriDispari.push(numeroUtente); // Se lo è, lo aggiungiamo all'array
+      }
 }  
